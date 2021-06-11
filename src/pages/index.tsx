7d2d355ea6a73fad832aba3fd7e532377c59f690
Gustavo Saboya <saboya.gustavo@gmail.com>
2021-06-11 07:41:57 -0300

    Structured the index page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,24 @@ export default function Home() {
       <Head>
         <title>HOME | ig.news</title>
       </Head>
+
+      <main>
+        <section>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about <br />
+            the <span>React</span> world!
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            for <span>$9.90 monthly</span>
+          </p>
+        </section>
+        <img
+          src='/images/avatar.svg'
+          alt='A young man sitting in a desk coding beside a coffee mug'
+        />
+      </main>
     </>
   );
 }
